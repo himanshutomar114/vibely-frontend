61704018c4492e44d5698d43ef85f40668f2238a
himanshutomar114 <himanshutomar114@gmail.com>
2025-07-03 23:42:56 +0530

    ax

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 //for deployment of website we use the base_url beacuse it changes the backend url on server
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5600/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5600/api" : "https://vibely-backend-f25o.onrender.com";
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL, //backend server url
