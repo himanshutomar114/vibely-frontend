815c25dd20463d67a8510c4e963ed049e4f4aff8
himanshutomar114 <himanshutomar114@gmail.com>
2025-07-03 23:08:12 +0530

    axio

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 //for deployment of website we use the base_url beacuse it changes the backend url on server
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5600/api" : "/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL, //backend server url
