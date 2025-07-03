e26c5ad809ff95aae69d41389fb8bb4ff6a0e4fa
himanshutomar114 <himanshutomar114@gmail.com>
2025-07-03 22:58:46 +0530

    axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,9 +2,11 @@
 //axios - perform HTTP requests like GET , POST , PUT, DELETE 
 import axios from "axios";
 
+//for deployment of website we use the base_url beacuse it changes the backend url on server
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5600/api" : "/api";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5600/api", //backend server url
+    baseURL: BASE_URL, //backend server url
     withCredentials: true //send cookies with request
 
 })
