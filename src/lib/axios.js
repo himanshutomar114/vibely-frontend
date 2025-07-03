ab02e1976d68f581462bc67cc88683151c03b781
himanshutomar114 <himanshutomar114@gmail.com>
2025-07-04 00:02:13 +0530

    aa

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,12 +1,11 @@
-
-//axios - perform HTTP requests like GET , POST , PUT, DELETE 
 import axios from "axios";
 
-//for deployment of website we use the base_url beacuse it changes the backend url on server
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5600/api" : "https://vibely-backend-f25o.onrender.com/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5600/api"
+    : "https://vibely-backend-f25o.onrender.com/api"; // ✅ Full Render URL in production
 
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL, //backend server url
-    withCredentials: true //send cookies with request
-
-})
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
